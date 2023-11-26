56f25884e9392f2315b3dbd4bcc40752874bfab1
Sarpreet <ssarpreet.786@gmail.com>
2023-11-26 14:25:53 -0500

    Adding Project Files

---
+++ CheckoutPage/script.js
@@ -0,0 +1,29 @@
+
+// document.addEventListener('DOMContentLoaded', function () {
+   
+//     const form = document.getElementById('formData');
+
+//     form.addEventListener('submit', function (event) {
+      
+//         if (!validateForm()) {
+//             event.preventDefault();
+//         }
+//     });
+
+
+//     function validateForm() {
+//         const firstName = document.getElementById('firstName').value;
+//         const lastName = document.getElementById('lastName').value;
+//         const email = document.getElementById('email').value;
+//         const address = document.getElementById('address').value;
+//         const creditCard = document.getElementById('creditCard').value;
+//         const expirationDate = document.gewstElementById('expirationDate').value;
+//         const cvv = document.getElementById('cvv').value;
+
+//         if (!firstName || !lastName || !email || !address || !creditCard || !expirationDate || !cvv) {
+//             alert('Please fill in all the required fields.');
+//             return false;
+//         }
+//         return true; 
+//     }
+// });
---
+++ LoginPage/script.js
@@ -0,0 +1,27 @@
+
+document.getElementById("button").addEventListener("click", () => {
+
+    const emailOrPhone = document.getElementById("exampleInputEmail1").value;
+    const password = document.getElementById("exampleInputPassword1").value;
+    
+    const userDataJSON = localStorage.getItem("userData");
+    if (userDataJSON) {
+        const userData = JSON.parse(userDataJSON);
+        if ((userData.email === emailOrPhone || userData.number ===emailOrPhone) && userData.password === password) {
+
+            window.location.href = "../Home/index.html";
+        } else {
+           
+            alert("Authentication failed. Please check your credentials.");
+        }
+    } else {
+       
+        alert("Authentication failed. User not found.");
+    }
+});
+
+document.getElementById("submit").addEventListener("click", () => {
+
+            window.location.href = "../Home/index.html";
+});
+
